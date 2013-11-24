1284b73ea106df8969dd35878b6248a5b87d5150
Jason Dusek <jason.dusek@gmail.com>
2013-11-24 07:08:53 +0000

    Add create/delete actions. Center form and routes.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,12 +12,19 @@ object Application extends Controller {
     Ok(views.html.index(Route.active.snapshot, Route.form))
   }
 
-  def newRoute = Action { implicit request =>
+  def handleForm = Action { implicit request =>
     Route.form.bindFromRequest.fold(
       errors => BadRequest(views.html.index(Route.active.snapshot, errors)),
-      route => {
-        Route.active.single += (route.path -> route)
-        Ok(views.html.index(Route.active.snapshot, Route.form))
+      form => {
+        form.action match {
+          case "create" => {
+            val route = Route(form.path, form.bucket, form.access, form.secret)
+            Route.active.single += (route.path -> route)
+          }
+          case "delete" => Route.active.single -= form.path
+        }
+        //Ok(views.html.index(Route.active.snapshot, Route.form))
+        Redirect(routes.Application.index())
       }
     )
   }
--- app/models/Route.scala
+++ app/models/Route.scala
@@ -8,14 +8,21 @@ import play.api.data.format.Formats._
 
 case class Route(path: String, bucket: String, access: String, secret: String)
 
+case class RouteChange(path: String,
+                       bucket: String,
+                       access: String,
+                       secret: String,
+                       action: String)
+
 object Route {
   val active: TMap[String, Route] = TMap()
-  val form: Form[Route] = Form(
+  val form: Form[RouteChange] = Form(
     mapping(
       "path"   -> of[String],
       "bucket" -> of[String],
       "access" -> of[String],
-      "secret" -> of[String]
-    )(Route.apply)(Route.unapply)
+      "secret" -> of[String],
+      "action" -> of[String]
+    )(RouteChange.apply)(RouteChange.unapply)
   )
 }
