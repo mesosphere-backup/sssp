0b499768ff08a6d716723e8b0ffdec7c20302dd1
Jason Dusek <jason.dusek@gmail.com>
2013-11-25 19:18:37 +0000

    Rethink regions.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,8 +2,6 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import com.amazonaws.regions.Region
-import com.amazonaws.regions.Regions._
 
 import mesosphere.sssp._
 import models._
@@ -12,17 +10,17 @@ import models._
 object Application extends Controller {
   val ssspRoutes: Routes = new Routes()
 
-  def index = Action {
-    Ok(views.html.index(routeChanges, RouteChange.form))
+  def index() = Action {
+    Ok(views.html.index(routesAsRouteChanges(), RouteChange.form))
   }
 
   def handleForm = Action { implicit request =>
     RouteChange.form.bindFromRequest.fold(
-      errors => BadRequest(views.html.index(routeChanges, errors)),
+      errors => BadRequest(views.html.index(routesAsRouteChanges(), errors)),
       change => {
         change.action match {
           case "create" => {
-            val region = Region.getRegion(DEFAULT_REGION)
+            val region = S3Notary.regions(change.region)
             val creds =
               new BasicAWSCredentialsProvider(change.access, change.secret)
             val notary = new S3Notary(change.bucket, region, creds)
@@ -34,12 +32,12 @@ object Application extends Controller {
             ssspRoutes -= path
           }
         }
-        Redirect(routes.Application.index())
+        Redirect(routes.Application.index)
       }
     )
   }
 
-  def routeChanges: Seq[RouteChange] =
+  def routesAsRouteChanges(): Seq[RouteChange] =
     for ((path, notary) <- ssspRoutes.toSeq) yield {
       val creds = notary.credentials.getCredentials()
       RouteChange("/" + path.mkString("/"),
@@ -49,4 +47,13 @@ object Application extends Controller {
                   creds.getAWSSecretKey,
                   "create")
     }
+
+  def regionsList(): Seq[String] = S3Notary.regions.keys.toSeq.sortWith {
+    // Ensure us-east-1 is the first, default selection.
+    case ("us-east-1", _) => true
+    case (_, "us-east-1") => false
+    // Sort backwards so the other US regions show up at the beginning, next
+    // to us-east-1. It looks more natural this way.
+    case (a, b)           => a > b
+  }
 }
--- src/main/scala/mesosphere/sssp/S3Notary.scala
+++ src/main/scala/mesosphere/sssp/S3Notary.scala
@@ -2,22 +2,23 @@ package mesosphere.sssp
 
 import com.amazonaws.HttpMethod
 import com.amazonaws.auth._
-import com.amazonaws.regions.Region
-import com.amazonaws.regions.Regions._
+import com.amazonaws.regions._
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
 import java.util.Date
 import java.net.URL
-import scala.concurrent.duration.Duration
-
+import scala.concurrent.duration._
 
 class S3Notary(val bucket: String,
-               val region: Region = Region.getRegion(DEFAULT_REGION),
+               val region: Region = Region.getRegion(Regions.US_EAST_1),
                val credentials: AWSCredentialsProvider =
-                   new EnvironmentVariableCredentialsProvider()) {
+                   new EnvironmentVariableCredentialsProvider(),
+               val defaultExpiration: Duration = 10 seconds) {
   var client: AmazonS3Client = null
 
-  def sign(key: String, method: HttpMethod, lifetime: Duration): URL = {
+  def sign(key: String,
+           method: HttpMethod,
+           lifetime: Duration = defaultExpiration): URL = {
     if (client == null) reconnect()
     val expires = new Date(System.currentTimeMillis() + lifetime.toMillis)
     val request = new GeneratePresignedUrlRequest(bucket, key, method)
@@ -29,4 +30,9 @@ class S3Notary(val bucket: String,
     client = new AmazonS3Client(credentials)
     client.setRegion(region)
   }
+}
+
+object S3Notary {
+  val regions: Map[String, Region] =
+    Regions.values.map(r => (r.getName -> Region.getRegion(r))).toMap
 }
