18f82fe74e6862822803ef5975530e7f6cc8e5a3
Connor Doyle <connor@mesosphere.io>
2014-01-14 10:16:45 -0800

    Added scalatest as a dependency for the test scope in the build definition.  Added a failing test for mesosphere.sssp.Routes.

---
+++ test/scala/mesosphere/sssp/test/RoutesSpec.scala
@@ -0,0 +1,33 @@
+package mesosphere.sssp.test
+
+import mesosphere.sssp.{ Routes, S3Notary }
+
+class RoutesSpec extends Spec {
+
+  case class Fixture() { val routes = new Routes() }
+
+  "RoutesSpec" should "select the deepest (most specific) route for a path" in {
+    val f = Fixture()
+    val notaryA = new S3Notary("bucketA")
+    val notaryB = new S3Notary("bucketB")
+    val notaryC = new S3Notary("bucketC")
+
+    val pathA = Seq("a", "b")
+    val pathB = Seq("a", "b", "c")
+    val pathC = Seq("a", "a", "c")
+
+    f.routes += pathA -> notaryA
+    f.routes += pathB -> notaryB
+    f.routes += pathC -> notaryC
+
+    val query1 = Seq("a", "b", "x")
+    f.routes.deepestHandler(query1) should equal (pathA -> notaryA)
+
+    val query2 = Seq("a", "b", "c", "x")
+    f.routes.deepestHandler(query2) should equal (pathB -> notaryB)
+
+    val query3 = Seq("a", "a", "c")
+    f.routes.deepestHandler(query3) should equal (pathC -> notaryC)
+  }
+	
+}
---
+++ test/scala/mesosphere/sssp/test/Spec.scala
@@ -0,0 +1,6 @@
+package mesosphere.sssp.test
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+trait Spec extends FlatSpec with ShouldMatchers
